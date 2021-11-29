c82e7c0673baa2b9f42d15d086370c9012bd8b43
ReAlpha39 <yunusw718@gmail.com>
2021-11-29 15:16:46 +0700

    update dataset url

--- get-data.sh
+++ get-data.sh
@@ -2,7 +2,7 @@
 
 mkdir -p corpus
 # download Dataset
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet \--save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1NAtp079h9FpQppNIxFJH5sMnnhDcLWwp' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1NAtp079h9FpQppNIxFJH5sMnnhDcLWwp" -O dataset.tar.gz && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet \--save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1BecwJD8OoPJrE4cmSbXUuGE8gKqKn69T' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1BecwJD8OoPJrE4cmSbXUuGE8gKqKn69T" -O dataset.tar.gz && rm -rf /tmp/cookies.txt
 tar xzvf dataset.tar.gz
 rm dataset.tar.gz
 mv dataset ./corpus
@@ -12,3 +12,5 @@ wget http://www.kecl.ntt.co.jp/icl/lirg/jparacrawl/release/1.0/spm_models/en-ja_
 tar xzvf en-ja_spm.tar.gz
 rm en-ja_spm.tar.gz
 mv enja_spm_models ./corpus
+
+
